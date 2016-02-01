37eeb1601dc20bb855ff8ae546a97e90d369d7a1
seanagibson <seanandgibson@gmail.com>
2016-02-01 15:21:30 -0800

    [feat]: adds user sign out

--- client/src/containers/home.jsx
+++ client/src/containers/home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import HomeNavBar from './../components/homenavbar';
 import JumboTronButton from './../components/JumboTronButton';
 import DashboardTiles from './../components/dashboardTiles';
+import {userLogout} from './../actions/actionsUserAuth';
 
-export default class Home extends React.Component {
+class Home extends React.Component {
   render() {
     return (
         <div>
       <div className="full">
-        <HomeNavBar/>
+        <HomeNavBar submitSignout={this.props.submitSignout}
+          user={this.props.userAuth}/>
         <JumboTronButton/>
       </div>
       <div className="homeInfo">
@@ -17,4 +20,20 @@ export default class Home extends React.Component {
       </div>
     )
   }
-};
+}
+
+function mapDispatchToProps(dispatch){
+   return {
+     submitSignout: function(){
+       dispatch(userLogout());
+     }
+   }
+}
+
+function mapStateToProps(state){
+  return {
+    userAuth: state.userStore.userAuth
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
