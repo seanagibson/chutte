5adf5a4f6c6116f634a3dc16a51acb7eb3955db0
seanagibson <seanandgibson@gmail.com>
2016-02-05 17:17:40 -0800

    [refactor]: adds bootstrap collapse styling

--- client/src/components/homenavbar.jsx
+++ client/src/components/homenavbar.jsx
@@ -10,22 +10,27 @@ export default class HomeNavbar extends React.Component {
    }
 
   render() {
-    let icon = (
-    <span>
-        <img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />
-    </span>)
+    let icon = (<span><img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /></span>);
 
     if(this.props.user.token === ''){
       return (
         <nav className="navbar navbar-default">
           <div className="container-fluid">
             <div className="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="chutte-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
               <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
             </div>
-            <ul className="nav navbar-nav navbar-right">
-              <li><SignUpModal/></li>
-              <li><SignInModal/></li>
-            </ul>
+            <div className="collapse navbar-collapse" id="chutte-navbar-collapse">
+              <ul className="nav navbar-nav navbar-right">
+                <li><SignUpModal/></li>
+                <li><SignInModal/></li>
+              </ul>
+            </div>
           </div>
         </nav>
       );
@@ -34,11 +39,19 @@ export default class HomeNavbar extends React.Component {
         <nav className="navbar navbar-default">
           <div className="container-fluid">
             <div className="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="chutte-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
               <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
             </div>
-            <ul className="nav navbar-nav navbar-right" >
-              <li id="signOut"  onClick={this.handleSignout.bind(this)}><a>Sign out</a></li>
-            </ul>
+            <div className="collapse navbar-collapse" id="chutte-navbar-collapse">
+              <ul className="nav navbar-nav navbar-right" >
+                <li id="signOut"  onClick={this.handleSignout.bind(this)}><a>Sign out</a></li>
+              </ul>
+            </div>
           </div>
         </nav>
       );
